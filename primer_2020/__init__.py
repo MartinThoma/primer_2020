9fcb7e6803a5d21f6762dbb26d6b6c91a0191c0f
Martin Thoma <info@martin-thoma.de>
2020-02-09 11:19:03 +0100

    REL v0.2.0
    ENH: CLI added
    ENH: factorize() added

---
+++ primer_2020/__init__.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+def factorize(number: int) -> List[int]:
+    """
+    Factorize the given number.
+
+    Parameters
+    ----------
+    number : int
+
+    Returns
+    -------
+    factors : List[int]
+
+    Examples
+    --------
+    >>> factorize(14)
+    [2, 7]
+    >>> factorize(-14)
+    [-1, 2, 7]
+    """
+    factors = []
+    if not isinstance(number, int):
+        raise ValueError(f"integer expected, got {type(number)}")
+    if number == 0:
+        raise ValueError("0 cannot be factorized.")
+    if number < 0:
+        number *= -1
+        factors.append(-1)
+    for factor in range(2, number + 1):
+        while number % factor == 0:
+            factors.append(factor)
+            number = int(number / factor)
+        if number == 1:
+            break
+    return factors
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
